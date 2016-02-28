78aa8142a267be2a355ab20ca19d587b8ed5b74e
Konstantinos Samaras-Tsakiris <kisamara@auth.gr>
2016-02-28 04:46:08 +0200

    Works correctly

--- proj3/src/kernels.cu
+++ proj3/src/kernels.cu
@@ -7,9 +7,9 @@ __global__ void fw(int* g, int N, int k){
 }
 
 template<int n>
-__global__ void phase1(int* g, int pstart){
+__global__ void phase1(int* g, const int pstart, const int N){
   __shared__ int tile[n][n];
-  tile[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*n + pstart+threadIdx.x ];
+  tile[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ];
   __syncthreads();
 
   for(int k=0; k<n; k++){
@@ -18,37 +18,44 @@ __global__ void phase1(int* g, int pstart){
     __syncthreads();
   }
 
-  g[ (pstart+threadIdx.y)*n + pstart+threadIdx.x ]= tile[threadIdx.y][threadIdx.x];
+  g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ]= tile[threadIdx.y][threadIdx.x];
 }
 
 template<int n>
-__global__ void phase2(int* g, int pstart, int primary_n){
+__global__ void phase2(int* g, const int pstart, const int primary_n, const int N){
   __shared__ int tile[n][n], primary[n][n];
-  int blkIdx_skip= (blockIdx.x > primary_n)? blockIdx.x+1: blockIdx.x;      // skip primary tile
+  int blkIdx_skip= (blockIdx.x >= primary_n)? blockIdx.x+1: blockIdx.x;      // skip primary tile
   int x_t= (blockIdx.y)? blkIdx_skip*n+threadIdx.x: pstart+threadIdx.x;     // tile coordinates
   int y_t= (blockIdx.y)? pstart+threadIdx.y: blkIdx_skip*n+threadIdx.y;     // blkIdx,y? row: col
-  primary[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*n + pstart+threadIdx.x ];
-  tile[threadIdx.y][threadIdx.x]= g[y_t*n + x_t];
+  primary[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ];
+  tile[threadIdx.y][threadIdx.x]= g[y_t*N + x_t];
   __syncthreads();
+  
+  if(blockIdx.y)
+    for(int k=0; k<n; k++){
+      if(tile[threadIdx.y][threadIdx.x] > primary[threadIdx.y][k]+tile[k][threadIdx.x])
+        tile[threadIdx.y][threadIdx.x]= primary[threadIdx.y][k]+tile[k][threadIdx.x];
+      __syncthreads();
+    }
+  else
+    for(int k=0; k<n; k++){
+      if(tile[threadIdx.y][threadIdx.x] > tile[threadIdx.y][k]+primary[k][threadIdx.x])
+        tile[threadIdx.y][threadIdx.x]= tile[threadIdx.y][k]+primary[k][threadIdx.x];
+      __syncthreads();
+    }
 
-  for(int k=0; k<n; k++){
-    if(tile[threadIdx.y][threadIdx.x] > primary[threadIdx.y][k]+primary[k][threadIdx.x])
-      tile[threadIdx.y][threadIdx.x]= primary[threadIdx.y][k]+primary[k][threadIdx.x];
-    __syncthreads();
-  }
-
-  g[y_t*n + x_t]= tile[threadIdx.y][threadIdx.x];
+  g[y_t*N + x_t]= tile[threadIdx.y][threadIdx.x];
 }
 
 template<int n>
-__global__ void phase3(int* g, int pstart, int primary_n){
+__global__ void phase3(int* g, const int pstart, const int primary_n, const int N){
   __shared__ int tile[n][n], row[n][n], col[n][n];
-  int blkIdx_xskip= (blockIdx.x > primary_n)? blockIdx.x+1: blockIdx.x;     // skip primary tile
-  int blkIdx_yskip= (blockIdx.y > primary_n)? blockIdx.y+1: blockIdx.y;
+  int blkIdx_xskip= (blockIdx.x >= primary_n)? blockIdx.x+1: blockIdx.x;     // skip primary tile
+  int blkIdx_yskip= (blockIdx.y >= primary_n)? blockIdx.y+1: blockIdx.y;
   int x_t= blkIdx_xskip*n+threadIdx.x, y_t= blkIdx_yskip*n+threadIdx.y;     // tile coordinates
-  row[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*n + x_t ];
-  col[threadIdx.y][threadIdx.x]= g[ y_t*n + pstart+threadIdx.x   ];
-  tile[threadIdx.y][threadIdx.x]= g[y_t*n + x_t];
+  row[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*N + x_t ];
+  col[threadIdx.y][threadIdx.x]= g[ y_t*N + pstart+threadIdx.x   ];
+  tile[threadIdx.y][threadIdx.x]= g[y_t*N + x_t];
   __syncthreads();
 
   for(int k=0; k<n; k++){
@@ -57,6 +64,35 @@ __global__ void phase3(int* g, int pstart, int primary_n){
     __syncthreads();
   }
 
-  g[y_t*n + x_t]= tile[threadIdx.y][threadIdx.x];
+  g[y_t*N + x_t]= tile[threadIdx.y][threadIdx.x];
+}
+
+template<int n>
+__global__ void copy1(int* g, const int pstart, const int N){
+  __shared__ int tile[n][n];
+  tile[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ];
+  __syncthreads();
+
+  tile[threadIdx.y][threadIdx.x]= -1;
+
+  g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ]= tile[threadIdx.y][threadIdx.x];
+}
+
+
+template<int n>
+__global__ void copy2(int* g, const int pstart, const int primary_n, const int N){
+  __shared__ int tile[n][n], primary[n][n];
+  int blkIdx_skip= (blockIdx.x >= primary_n)? blockIdx.x+1: blockIdx.x;      // skip primary tile
+  int x_t= (blockIdx.y)? blkIdx_skip*n+threadIdx.x: pstart+threadIdx.x;     // tile coordinates
+  int y_t= (blockIdx.y)? pstart+threadIdx.y: blkIdx_skip*n+threadIdx.y;     // blkIdx,y? row: col
+  primary[threadIdx.y][threadIdx.x]= g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ];
+  tile[threadIdx.y][threadIdx.x]= g[y_t*N + x_t];
+  __syncthreads();
+
+
+  tile[threadIdx.y][threadIdx.x]= -1;
+  primary[threadIdx.y][threadIdx.x]= -2;
+  g[y_t*N + x_t]= tile[threadIdx.y][threadIdx.x];
+  g[ (pstart+threadIdx.y)*N + pstart+threadIdx.x ]= primary[threadIdx.y][threadIdx.x];
 }
 
--- proj3/src/main.cu
+++ proj3/src/main.cu
@@ -4,7 +4,7 @@
 #include "kernels.cu"
 using namespace std;
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+#define gpuErrchk(ans) gpuAssert((ans), __FILE__, __LINE__)
 #define MAX_THRpBLK2D 32
 
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
@@ -18,17 +18,26 @@ enum Dir { H2D, D2H };
 
 template<class T>
 struct DPtr{
-  DPtr(int N) { gpuErrchk(cudaMalloc(&data_, N*sizeof(T))) }
+  DPtr(int N) { gpuErrchk(cudaMalloc(&data_, N*sizeof(T))); }
   ~DPtr() { cudaFree(data_); }
   void copy(T* a, int N, Dir dir) {
-    if(dir == Dir::H2D) gpuErrchk(cudaMemcpy(data_, a, sizeof(T)*N, cudaMemcpyHostToDevice))
-    else gpuErrchk(cudaMemcpy(a, data_, sizeof(T)*N, cudaMemcpyDeviceToHost))
+    if(dir == Dir::H2D) gpuErrchk(cudaMemcpy(data_, a, sizeof(T)*N, cudaMemcpyHostToDevice));
+    else gpuErrchk(cudaMemcpy(a, data_, sizeof(T)*N, cudaMemcpyDeviceToHost));
   }
   T* get() { return data_; }
   operator T*() { return data_; }
 private:
   T* data_;
 };
+
+void printG(unique_ptr<int[]>& g, int N){
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++)
+      printf("%3d\t", g[i*N+j]);
+    printf("\n");
+  }
+  printf("_____________________________________\n");
+}
 
 int main(){
   int N;
@@ -39,14 +48,7 @@ int main(){
     for(int j=0; j<N; j++)
       scanf("%d", &g[i*N+j]);
   printf("\nN=%d\n", N);
-/*
-  for(int i=0; i<N;i++){
-    for(int j=0;j<N;j++)
-      printf("%3d ", g[i*N+j]);
-    printf("\n");
-  }
-    printf("\n\n");
-*/
+
   // CPU Floyd-Warshall
   unique_ptr<int[]> result_cpu(new int[N*N]);
   for(int i=0; i<N*N; i++) result_cpu[i]= g[i];
@@ -55,32 +57,32 @@ int main(){
       for(int j=0; j<N; j++)
         if(result_cpu[i*N+j] > result_cpu[i*N+k]+result_cpu[k*N+j])
           result_cpu[i*N+j]= result_cpu[i*N+k]+result_cpu[k*N+j];
-printf("CPU calc done\n");
+  printf("CPU calc done\n");
 
   // simple GPU Floyd-Warshall
+  unique_ptr<int[]> result_simple(new int[N*N]);
   d_g.copy(g.get(), N*N, Dir::H2D);
   dim3 bs(MAX_THRpBLK2D, MAX_THRpBLK2D);
-  if(N<MAX_THRpBLK2D) bs= (N,N);
+  if(N<MAX_THRpBLK2D) bs= dim3(N,N);
   dim3 gs(N/bs.x, N/bs.y);
-printf("Launching simple kernel...\n");
+  printf("Launching GPU simple algo...\n");
   for(int k=0; k<N; k++) fw<<<gs,bs>>>(d_g, N, k);
-  unique_ptr<int[]> result_simple(new int[N*N]);
   d_g.copy(result_simple.get(), N*N, Dir::D2H);
-printf("GPU simple kernel done\n");
+  printf("GPU simple kernel done\n");
 
   // GPU block algo
-  d_g.copy(g.get(), N*N, Dir::H2D);
+  unique_ptr<int[]> result_block(new int[N*N]);
   constexpr const int n= MAX_THRpBLK2D;
   const int B= N/n;
-printf("Launching GPU block kernel, B=%d\n", B);
+  d_g.copy(g.get(), N*N, Dir::H2D);
+  printf("Launching GPU block algo, B=%d\n", B);
   for(int b=0; b<B; b++){
-    phase1<n> <<<1,bs>>>(d_g, b*n);
-    phase2<n> <<<(B-1,2),bs>>>(d_g, b*n, b);
-    phase3<n> <<<(B-1,B-1),bs>>>(d_g, b*n, b);
+    phase1<n> <<<1,bs>>>(d_g, b*n, N);
+    phase2<n> <<<dim3(B-1,2),bs>>>(d_g, b*n, b, N);
+    phase3<n> <<<dim3(B-1,B-1),bs>>>(d_g, b*n, b, N);
   }
-  unique_ptr<int[]> result_block(new int[N*N]);
   d_g.copy(result_block.get(), N*N, Dir::D2H);
-printf("GPU block kernel done\n");
+  printf("GPU block kernel done\n");
 
   for(int i=0; i<N; i++)
     for(int j=0; j<N; j++)
@@ -96,6 +98,7 @@ printf("GPU block kernel done\n");
             result_cpu[i*N+j]);
         exit(1);
       }
+  printf("[check]: Test success!\n");
 
   return 0;
 }
